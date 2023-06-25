aebf18822c956349167325a7bcc2ea2c4bc55935
Neo <lordneobarnachea@gmail.com>
2023-06-26 07:09:55 +0800

    fix mui widhth

--- admin/src/pages/EquipManage.jsx
+++ admin/src/pages/EquipManage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button, TextField } from '@mui/material'
+import { Form, Link } from 'react-router-dom'
+import { Button, TextField, Divider } from '@mui/material'
 import '../styles/equipManage.css'
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 export default function EquipManage() {
     return (
         <>
@@ -12,11 +16,13 @@ export default function EquipManage() {
                     </div>
                     <div>
                         <TextField
+                            sx={{ width: '100%', maxWidth: '75%' }}
                             label='Equipment name'
                         />
                     </div>
                     <div>
                         <TextField
+                            sx={{ width: '100%', maxWidth: '75%' }}
                             label='Quantity'
                             type='number'
                         />
@@ -25,18 +31,26 @@ export default function EquipManage() {
                         <Button variant='contained'>Done</Button>
                     </div>
                 </div>
-
+                <Divider />
                 <div className='remove-item sameq'>
                     <div className='item-label'>
                         Remove Item
                     </div>
                     <div>
-                        <TextField
-                            label='Equipment name'
-                        />
+                        <FormControl sx={{ width: '100%', maxWidth: '75%' }}>
+                            <InputLabel id="demo-simple-select-helper-label">Select Item</InputLabel>
+                            <Select
+                                label='Select Item'
+                            >
+                                <MenuItem value={10}>Chair</MenuItem>
+                                <MenuItem value={20}>Tent</MenuItem>
+                                <MenuItem value={30}>Table</MenuItem>
+                            </Select>
+                        </FormControl>
                     </div>
                     <div>
                         <TextField
+                            sx={{ width: '100%', maxWidth: '75%' }}
                             label='Quantity'
                             type='number'
                         />
@@ -45,17 +59,26 @@ export default function EquipManage() {
                         <Button variant='contained'>Done</Button>
                     </div>
                 </div>
+                <Divider />
                 <div className='update-item sameq'>
                     <div className='item-label'>
                         Update Item
                     </div>
                     <div>
-                        <TextField
-                            label='Equipment name'
-                        />
+                        <FormControl sx={{ width: '100%', maxWidth: '75%' }}>
+                            <InputLabel id="demo-simple-select-helper-label">Select Item</InputLabel>
+                            <Select
+                                label='Select Item'
+                            >
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
                     </div>
                     <div>
                         <TextField
+                            sx={{ width: '100%', maxWidth: '75%' }}
                             label='Quantity'
                             type='number'
                         />                    </div>
