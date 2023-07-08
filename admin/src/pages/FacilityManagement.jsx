e8aee45d207262250637df08e9fbe5e05930daf0
Neo <lordneobarnachea@gmail.com>
2023-07-08 19:12:23 +0800

    added ui to the facility component

--- admin/src/pages/FacilityManagement.jsx
+++ admin/src/pages/FacilityManagement.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import FormManagement from '../form-components/FormManagement'
+import { Divider } from '@mui/material'
+const Container = ({children}) =>{
+  const style = {
+    width: '100%',
+    height: '100%',
+    border: '1px solid black',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '2em'
+  }
+  return(
+    <div style={style}>
+      {children}
+    </div>
+  )
+}
 
 export default function FacilityManagement() {
+
   return (
-    <div>
-      <h1>Facility Management</h1>
-    </div>
+    <>
+      <Container>
+      <FormManagement formTitle='Add Certificate'/>
+      <Divider/>
+      <FormManagement formTitle='Add Certificate'/>
+      <Divider/>
+      <FormManagement formTitle='Add Certificate'/>
+      </Container>
+    </>
   )
 }
