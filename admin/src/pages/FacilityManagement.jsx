48acceb82a0e4bfb34cb17dae33bc0eaaf5ac2c4
Neo <lordneobarnachea@gmail.com>
2023-07-10 21:08:01 +0800

    added UI for facility and medicine component

--- admin/src/pages/FacilityManagement.jsx
+++ admin/src/pages/FacilityManagement.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import FormManagement from '../form-components/FormManagement'
-import { Divider } from '@mui/material'
+import { Divider, TextField, Button, Select, MenuItem } from '@mui/material'
+
 const Container = ({children}) =>{
   const style = {
     width: '100%',
-    height: '100%',
-    border: '1px solid black',
+    height: '95%',
+    // border: '1px solid black',
     display: 'flex',
     flexDirection: 'column',
     gap: '2em'
@@ -17,17 +18,92 @@ const Container = ({children}) =>{
   )
 }
 
+const divStyle ={
+  // border: '1px solid red',
+  height: '33%',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center'
+}
+const itemStyle={
+  // border: '1px solid red',
+  width: '25%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
 export default function FacilityManagement() {
-
   return (
     <>
-      <Container>
-        <FormManagement formTitle='Add Certificate'/>
+     <Container>
+        <div style={divStyle}>
+
+          <div className='item-label' style={itemStyle}>
+            Add Facility
+          </div>
+          <div style={itemStyle}>
+            <TextField
+            label="Add Facility"
+          />
+          </div>
+
+          <div style={itemStyle}>
+            <TextField
+            label="Quantity"
+          />
+          </div>
+
+          <div  style={itemStyle}>
+            <Button variant="contained">Okay</Button>
+          </div>
+
+        </div>
         <Divider/>
-        <FormManagement formTitle='Add Certificate'/>
+        <div style={divStyle}>
+        <div className='item-label' style={itemStyle}>
+           Remove Facility
+          </div>
+          <div style={itemStyle}>
+            <TextField
+            label="Remove Facility"
+          />
+          </div>
+
+          <div style={itemStyle}>
+            <TextField
+            label="Quantity"
+          />
+          </div>
+
+          <div  style={itemStyle}>
+            <Button variant="contained">Okay</Button>
+          </div>
+
+        </div>
         <Divider/>
-        <FormManagement formTitle='Add Certificate'/>
-      </Container>
+
+        <div style={divStyle}>
+        <div className='item-label' style={itemStyle}>
+            Update Facility
+          </div>
+          <div style={itemStyle}>
+            <TextField
+            label="Update Facility"
+          />
+          </div>
+
+          <div style={itemStyle}>
+            <TextField
+            label="Quantity"
+          />
+          </div>
+
+          <div  style={itemStyle}>
+            <Button variant="contained">Okay</Button>
+          </div>
+
+        </div>
+     </Container>
     </>
   )
 }
--- admin/src/pages/MedicineManagement.jsx
+++ admin/src/pages/MedicineManagement.jsx
@@ -5,8 +5,8 @@ import { Divider, TextField, Button, Select, MenuItem } from '@mui/material'
 const Container = ({children}) =>{
   const style = {
     width: '100%',
-    height: '100%',
-    border: '1px solid black',
+    height: '95%',
+    // border: '1px solid black',
     display: 'flex',
     flexDirection: 'column',
     gap: '2em'
@@ -18,11 +18,91 @@ const Container = ({children}) =>{
   )
 }
 
+const divStyle ={
+  // border: '1px solid red',
+  height: '33%',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center'
+}
+const itemStyle={
+  // border: '1px solid red',
+  width: '25%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
 export default function MedicineManagement() {
   return (
     <>
      <Container>
-        <FormManagement formTitle='Add Certificates'/>
+        <div style={divStyle}>
+
+          <div className='item-label' style={itemStyle}>
+            Add Medicine
+          </div>
+          <div style={itemStyle}>
+            <TextField
+            label="Add Medicine"
+          />
+          </div>
+
+          <div style={itemStyle}>
+            <TextField
+            label="Quantity"
+          />
+          </div>
+
+          <div  style={itemStyle}>
+            <Button variant="contained">Okay</Button>
+          </div>
+
+        </div>
+        <Divider/>
+        <div style={divStyle}>
+        <div className='item-label' style={itemStyle}>
+            Remove Medicine
+          </div>
+          <div style={itemStyle}>
+            <TextField
+            label="Remove Medicine"
+          />
+          </div>
+
+          <div style={itemStyle}>
+            <TextField
+            label="Quantity"
+          />
+          </div>
+
+          <div  style={itemStyle}>
+            <Button variant="contained">Okay</Button>
+          </div>
+
+        </div>
+        <Divider/>
+
+        <div style={divStyle}>
+        <div className='item-label' style={itemStyle}>
+            Update Medicine
+          </div>
+          <div style={itemStyle}>
+            <TextField
+            label="Update Medicine"
+          />
+          </div>
+
+          <div style={itemStyle}>
+            <TextField
+            label="Quantity"
+          />
+          </div>
+
+          <div  style={itemStyle}>
+            <Button variant="contained">Okay</Button>
+          </div>
+
+        </div>
      </Container>
     </>
   )
