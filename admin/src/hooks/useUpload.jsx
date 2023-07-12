6c7d1bfed04a1e62318a009eb0ab62b25f62e40e
Neo <lordneobarnachea@gmail.com>
2023-07-12 12:43:50 +0800

    added CRUD in Medicine component

--- admin/src/hooks/useUpload.jsx
+++ admin/src/hooks/useUpload.jsx
@@ -2,13 +2,13 @@ import { useEffect } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../firebase';
 
-const useUpload = async (formName, formNumber, path) => {
+const useUpload = async (name, number, path) => {
   try{
     await addDoc(collection(db, path),{
-      type: formName,
-      quantity: formNumber
+      type: name,
+      quantity: number
     })
-    console.log("Certificate Uploaded!")
+    console.log("Uploaded!")
   }catch(error){
     console.log(error)
   }
--- admin/src/pages/MedicineManagement.jsx
+++ admin/src/pages/MedicineManagement.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import FormManagement from '../form-components/FormManagement'
-import { Divider, TextField, Button, Select, MenuItem } from '@mui/material'
+import { Divider, TextField, Button, Select, MenuItem, FormControl, InputLabel } from '@mui/material'
+import { useState } from 'react'
+import useUpload from '../hooks/useUpload'
+import useRead from '../hooks/useRead'
+import useUpdate from '../hooks/useUpdate'
+import useDelete from '../hooks/useDelete'
 
-const Container = ({children}) =>{
+
+const Container = ({ children }) => {
   const style = {
     width: '100%',
     height: '95%',
@@ -11,99 +17,164 @@ const Container = ({children}) =>{
     flexDirection: 'column',
     gap: '2em'
   }
-  return(
+  return (
     <div style={style}>
       {children}
     </div>
   )
 }
 
-const divStyle ={
+const divStyle = {
   // border: '1px solid red',
   height: '33%',
   display: 'flex',
   justifyContent: 'space-around',
   alignItems: 'center'
 }
-const itemStyle={
+const itemStyle = {
   // border: '1px solid red',
   width: '25%',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center'
 }
+
+
 export default function MedicineManagement() {
+  const [name, setName] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState('');
+
+  useRead('Medicines', setData);
+
+  const Add = async (e) => {
+    e.preventDefault()
+    console.log(name, quantity)
+    await useUpload(name, quantity, 'Medicines').then(
+      setName(''),
+      setQuantity('')
+    )
+  }
+
+  const Delete = async (e) => {
+    e.preventDefault();
+    await useDelete('Medicines', value).then(
+    setValue(''),
+    console.log('success!')
+    )
+  }
+
+  const Update = async (e) => {
+    e.preventDefault();
+    console.log(value)
+    await useUpdate('Medicines', value).then(
+      setValue(''),
+      console.log('success')
+    )
+  }
+
+  const items = data.map((item) => {
+    return(
+    <MenuItem key={item.id} value={item.id}>
+      {item.type}
+    </MenuItem>
+    )
+  })
+
   return (
     <>
-     <Container>
-        <div style={divStyle}>
+      <Container>
+        <form style={divStyle} onSubmit={Add}>
 
           <div className='item-label' style={itemStyle}>
             Add Medicine
           </div>
           <div style={itemStyle}>
             <TextField
-            label="Add Medicine"
-          />
+              label="Add Medicine"
+              value={name}
+              onChange={
+                (e) => {
+                  setName(e.target.value)
+                }
+              }
+            />
           </div>
 
           <div style={itemStyle}>
             <TextField
-            label="Quantity"
-          />
+              value={quantity}
+              label="Quantity"
+              type="number"
+              onChange={(e) => setQuantity(e.target.value)}
+            />
           </div>
 
-          <div  style={itemStyle}>
-            <Button variant="contained">Okay</Button>
+          <div style={itemStyle}>
+            <Button variant="contained" type='submit'>Okay</Button>
           </div>
 
-        </div>
-        <Divider/>
-        <div style={divStyle}>
-        <div className='item-label' style={itemStyle}>
+        </form>
+        <Divider />
+
+        <form style={divStyle} onSubmit={Delete}>
+          <div className='item-label' style={itemStyle}>
             Remove Medicine
           </div>
           <div style={itemStyle}>
-            <TextField
-            label="Remove Medicine"
-          />
+            <FormControl sx={{ width: '100%', maxWidth: '75%' }}>
+              <InputLabel >Select Medicine</InputLabel>
+              <Select
+                label='Select Medicine'
+                value={value}
+                onChange={(e)=> setValue(e.target.value)}
+              >
+                {items}
+              </Select>
+            </FormControl>
           </div>
 
           <div style={itemStyle}>
-            <TextField
-            label="Quantity"
-          />
+
           </div>
 
-          <div  style={itemStyle}>
-            <Button variant="contained">Okay</Button>
+          <div style={itemStyle}>
+            <Button variant="contained" type='submit'>Okay</Button>
           </div>
 
-        </div>
-        <Divider/>
+        </form>
+        <Divider />
 
-        <div style={divStyle}>
-        <div className='item-label' style={itemStyle}>
+        <form style={divStyle} onSubmit={Update}>
+          <div className='item-label' style={itemStyle}>
             Update Medicine
           </div>
           <div style={itemStyle}>
-            <TextField
-            label="Update Medicine"
-          />
+            <FormControl sx={{ width: '100%', maxWidth: '75%' }}>
+              <InputLabel >Select Medicine</InputLabel>
+              <Select
+                label='Select Medicine'
+                value={value}
+                onChange={(e)=> setValue(e.target.value)}
+              >
+                {items}
+              </Select>
+            </FormControl>
           </div>
 
           <div style={itemStyle}>
             <TextField
-            label="Quantity"
-          />
+              label="Quantity"
+            />
           </div>
 
-          <div  style={itemStyle}>
-            <Button variant="contained">Okay</Button>
+          <div style={itemStyle}>
+            <Button variant="contained" type="submit">Okay</Button>
           </div>
 
-        </div>
-     </Container>
+        </form>
+      </Container>
     </>
   )
 }
