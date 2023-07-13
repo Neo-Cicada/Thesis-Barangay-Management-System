12702cc99a39ca6265f8efad6c84c22fa141ef31
Neo <lordneobarnachea@gmail.com>
2023-07-13 09:33:20 +0800

    addd responsive template

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 
 function App() {
@@ -6,7 +5,24 @@ function App() {
 
   return (
     <>
-      Hello world
+      <header>
+        <div className='header-container'>
+          <div>Logo</div>
+          <div>Home</div>
+
+
+        </div>
+      </header>
+      <main>
+        <div className='main-container'>
+
+        </div>
+      </main>
+      <footer>
+        <div className='footer-container'>
+          
+        </div>
+      </footer>
     </>
   )
 }
