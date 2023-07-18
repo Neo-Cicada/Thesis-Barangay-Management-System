49fca635a9b5ec26e3299b05ee03be3c93fcad89
Neo <lordneobarnachea@gmail.com>
2023-07-18 09:43:51 +0800

    added time picker for equipment

--- user/src/formComponents/EquipmentForm.jsx
+++ user/src/formComponents/EquipmentForm.jsx
@@ -1,36 +1,41 @@
 import React from 'react'
-import { TextField, Box, Grid, FormControl, Select, MenuItem, InputLabel, TextareaAutosize,
-  FormControlLabel, Checkbox, FormGroup, Button,  } from '@mui/material'
+import {
+  TextField, Box, Grid, FormControl, Select, MenuItem, InputLabel, TextareaAutosize,
+  FormControlLabel, Checkbox, FormGroup, Button,
+} from '@mui/material'
 import { useState } from 'react'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import useUpload from '../hooks/useUpload';
-  export default function EquipmentForm() {
+export default function EquipmentForm() {
 
-    const [firstname, setFirstName] = useState('');
-    const [lastname, setLastName] = useState('');
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [email, setEmail] = useState('');
-    const [equipment, setEquipment] = useState('');
-    const [messege, setMessege] = useState('');
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [equipment, setEquipment] = useState('');
+  const [messege, setMessege] = useState('');
 
-    const formData = {
-      firstname: firstname,
-      lastname: lastname,
-      startDate: startDate,
-      endDate: endDate,
-      phoneNumber: phoneNumber,
-      email: email,
-      equipment: equipment,
-      message: messege,
-    }
-  const style ={
+  const formData = {
+    firstname: firstname,
+    lastname: lastname,
+    startDate: startDate,
+    endDate: endDate,
+    phoneNumber: phoneNumber,
+    email: email,
+    equipment: equipment,
+    message: messege,
+  }
+  const style = {
     display: 'flex',
     justifyContent: 'center',
     height: '14%',
     alignItems: 'center'
   }
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     useUpload(formData, 'EquipmentRequest').then('upload successfully')
 
@@ -47,47 +52,48 @@ import useUpload from '../hooks/useUpload';
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <Box sx={{...style, gap: '4em'}}>
+        <Box sx={{ ...style, gap: '4em' }}>
           <TextField
-             value={firstname}
-             onChange={(e)=>setFirstName(e.target.value)}
-             label="Firstname" />
+            value={firstname}
+            onChange={(e) => setFirstName(e.target.value)}
+            label="Firstname" />
           <TextField
-             value={lastname}
-             onChange={(e)=>setLastName(e.target.value)}
-             label="Lastname" />
+            value={lastname}
+            onChange={(e) => setLastName(e.target.value)}
+            label="Lastname" />
 
         </Box>
-        <Box sx={{...style, gap: '4em'}}>
+        <Box sx={{ ...style, gap: '4em' }}>
           <TextField
             value={phoneNumber}
-            onChange={(e)=>setPhoneNumber(e.target.value)}
+            onChange={(e) => setPhoneNumber(e.target.value)}
             label="Phone Number" />
           <TextField
-            onChange={(e)=>setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             value={email}
             label="Email address" />
         </Box>
-        <Box sx={{...style, gap: '4em'}}>
-          <TextField
-            value={startDate}
-            onChange={(e)=>setStartDate(e.target.value)}
-            // label='date'
-            type='date'
+        <Box sx={{ ...style, gap: '4em' }}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker label="Start Date"
+              value={startDate}
+              onChange={(e)=>setStartDate(e.target.value)}
             />
-          <TextField
-            value={endDate}
-            onChange={(e)=>setEndDate(e.target.value)}
-            // label="End Date"
-            type='time'
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+              label="End Date"
             />
+          </LocalizationProvider>
         </Box>
-        <Box sx={{...style, gap: '4em'}}>
+        <Box sx={{ ...style, gap: '4em' }}>
           <FormControl fullWidth>
             <InputLabel >Select Available Equipment</InputLabel>
             <Select
               value={equipment}
-              onChange={(e)=>setEquipment(e.target.value)}
+              onChange={(e) => setEquipment(e.target.value)}
               label="Select Available Equipment">
               <MenuItem value="Equiment">Test</MenuItem>
             </Select>
@@ -95,16 +101,16 @@ import useUpload from '../hooks/useUpload';
         </Box>
         <Box sx={style}>
           <TextareaAutosize
-          value={messege}
-            onChange={(e)=>setMessege(e.target.value)}
-            style={{ resize: 'none', height: '100%', width: '100%', fontSize: '1.2rem', paddingLeft: '.8em'}} // Disable resizing
+            value={messege}
+            onChange={(e) => setMessege(e.target.value)}
+            style={{ resize: 'none', height: '100%', width: '100%', fontSize: '1.2rem', paddingLeft: '.8em' }} // Disable resizing
             placeholder="Messege"
             aria-label="fixed size textarea"
           />
         </Box>
         <Box sx={style}>
 
-            <FormControlLabel required control={<Checkbox />} label="Agree" />
+          <FormControlLabel required control={<Checkbox />} label="Agree" />
 
         </Box>
         <Box sx={style}>
