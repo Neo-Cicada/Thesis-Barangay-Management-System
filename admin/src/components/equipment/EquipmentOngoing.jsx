fe8115b20ec74a5869fa83473267510427709d0e
Neo <lordneobarnachea@gmail.com>
2023-07-23 10:43:52 +0800

    can now fetch data in ongoing status of equipment

--- admin/src/components/equipment/EquipmentOngoing.jsx
+++ admin/src/components/equipment/EquipmentOngoing.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import useRead from '../../hooks/useRead'
 import {useState} from 'react'
+import RequestBox from '../RequestBox'
+import { Container } from '@mui/material'
 export default function EquipmentOngoing() {
   const [data, setData] = useState([])
-  
+
+  useRead('EquipmentOngoingStatus', setData)
+
+  const items = data.map((item)=><li key={item.id}>
+  {item.id}
+
+  </li>)
   return (
     <>
-      ongoing
+      <Container>
+        {items}
+      </Container>
     </>
   )
 }
