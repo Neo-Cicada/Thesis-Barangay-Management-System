a08228dcd1fa866c34a1a8b2124138b18ddc01dd
Neo <lordneobarnachea@gmail.com>
2023-07-26 10:23:54 +0800

    created status component and added it on AppRoutes

--- admin/src/routes/AppRoutes.jsx
+++ admin/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Routes, Navigate  } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Certificate from '../components/certificate/Certificate'
 import Announcement from '../components/announcement/Announcement'
 import Enrollment from '../components/enrollment/Enrollment'
@@ -20,89 +20,107 @@ import CertificateManagement from '../components/certificate/CertificateManageme
 import CertificateRequest from '../components/certificate/CertificateRequest'
 import CertificateStatus from '../components/certificate/CertificateStatus'
 
+import CertificateOngoing from '../components/certificate/CertificateOngoing'
+import CertificateAccepted from '../components/certificate/CertificateAccepted'
+import CertificateRejected from '../component/certifcate/CertificateRejected'
+
 import MedicineManagement from '../components/medicine/MedicineManagement'
 import MedicineRequestList from '../components/medicine/MedicineRequestList'
 import MedicineStatus from '../components/medicine/MedicineStatus'
+
+import MedicineOngoing from '../components/medicine/MedicineOngoing'
+import MedicineCompleted from '../components/medicine/MedicineCompleted'
+import MedicineRejected from '../components/medicine/MedicineRejected'
 
 import FacilityManagement from '../components/facilities/FacilityManagement'
 import FacilityRequest from '../components/facilities/FacilityRequest'
 import FacilityStatus from '../components/facilities/FacilityStatus'
 
+import FacilityOngoing from '../components/facilities/FacilityOngoing'
+import FacilityCompleted from '../components/facilities/FacilityCompleted'
+import FacilityRejected from '../components/facilities/FacilityRejected'
+
 import AnnouncementManagement from '../components/announcement/AnnouncementManagement'
 import AnnouncementHistory from '../components/announcement/AnnouncementHistory'
-const AppRoutes = () =>{
-    return(
-        <Routes>
-                <Route path='/' element={<Equipment />}>
-                  <Route
-                    path='/'
-                    element={<EquipManage />}
-                  />
-                  <Route
-                    path='request'
-                    element={<EquipmentRequestList />}
-                  />
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Equipment />}>
+        <Route
+          path='/'
+          element={<EquipManage />}
+        />
+        <Route
+          path='request'
+          element={<EquipmentRequestList />}
+        />
 
-                  <Route
-                    path='status'
-                    element={<EquipmentStatus />}
-                  >
-                    <Route
-                      path='ongoing'
-                      element={<EquipmentOngoing/>}
-                    />
-                    <Route
-                      path='completed'
-                      element={<EquipmentCompleted/>}
-                    />
-                    <Route
-                      path='rejected'
-                      element={<EquipmentRejected/>}
-                    />
-                    <Route index element={<Navigate to='ongoing' replace />} />
-
-
-                  </Route>
-                </Route>
-                <Route path='certificate' element={<Certificate />} >
-                      <Route path='manage-certificate' element={<CertificateManagement/>}/>
-                      <Route index element={<Navigate to='manage-certificate' replace/>}/>
-                      <Route path ='request-list-certificate' element={<CertificateRequest/>}/>
-                      <Route path='certificate-status' element={<CertificateStatus/>}/>
-
-                </Route>
-
-                <Route path='announcement' element={<Announcement />}>
-                    <Route path="announcement-management" element={<AnnouncementManagement/>}/>
-                    <Route path="announcement-history" element={<AnnouncementHistory/>} />
-                    <Route index element={<Navigate to="announcement-management"/>} />
-                </Route>
+        <Route
+          path='status'
+          element={<EquipmentStatus />}
+        >
+          <Route
+            path='ongoing'
+            element={<EquipmentOngoing />}
+          />
+          <Route
+            path='completed'
+            element={<EquipmentCompleted />}
+          />
+          <Route
+            path='rejected'
+            element={<EquipmentRejected />}
+          />
+          <Route index element={<Navigate to='ongoing' replace />} />
 
 
+        </Route>
+      </Route>
+      <Route path='certificate' element={<Certificate />} >
+        <Route path='manage-certificate' element={<CertificateManagement />} />
+        <Route index element={<Navigate to='manage-certificate' replace />} />
+        <Route path='request-list-certificate' element={<CertificateRequest />} />
+        <Route path='certificate-status' element={<CertificateStatus />}>
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+
+        </Route>
+
+      </Route>
+
+      <Route path='announcement' element={<Announcement />}>
+        <Route path="announcement-management" element={<AnnouncementManagement />} />
+        <Route path="announcement-history" element={<AnnouncementHistory />} />
+        <Route index element={<Navigate to="announcement-management" />} />
+      </Route>
 
 
 
-                <Route path='enrollment' element={<Enrollment />} />
-
-                <Route path='facilities' element={<Facilities />}>
-                    <Route path="facility-management" element={<FacilityManagement/>} />
-                    <Route index element={<Navigate to='facility-management' replace/>}/>
-                    <Route path="facility-request-list" element={<FacilityRequest/>}/>
-                    <Route path="facility-status" element={<FacilityStatus/>}/>
-                </Route>
-
-                <Route path='medicine' element={<Medicine />} >
-                  <Route path='manage-medicine' element={<MedicineManagement/>} />
-                  <Route index element={<Navigate to='manage-medicine' replace/>} />
-
-                  <Route path='request-list-medicine' element={<MedicineRequestList/>} />
-                  <Route path='medicine-status' element={<MedicineStatus/>} />
-
-                </Route>
 
 
-              </Routes>
-    )
+      <Route path='enrollment' element={<Enrollment />} />
+
+      <Route path='facilities' element={<Facilities />}>
+        <Route path="facility-management" element={<FacilityManagement />} />
+        <Route index element={<Navigate to='facility-management' replace />} />
+        <Route path="facility-request-list" element={<FacilityRequest />} />
+        <Route path="facility-status" element={<FacilityStatus />} />
+      </Route>
+
+      <Route path='medicine' element={<Medicine />} >
+        <Route path='manage-medicine' element={<MedicineManagement />} />
+        <Route index element={<Navigate to='manage-medicine' replace />} />
+
+        <Route path='request-list-medicine' element={<MedicineRequestList />} />
+        <Route path='medicine-status' element={<MedicineStatus />} />
+
+      </Route>
+
+
+    </Routes>
+  )
 }
 
 export default AppRoutes
