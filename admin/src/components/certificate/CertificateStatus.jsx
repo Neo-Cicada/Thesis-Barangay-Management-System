784f0a3dc7e37a88ac15ab8b776d83a6bed52dd1
Neo <lordneobarnachea@gmail.com>
2023-07-26 10:03:27 +0800

    added status component

--- admin/src/components/certificate/CertificateStatus.jsx
+++ admin/src/components/certificate/CertificateStatus.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import '../../styles/eqStatus.css'
+import { Link, Outlet } from 'react-router-dom'
 export default function CertificateStatus() {
   return (
-    <div>
-      Certificate Status
-    </div>
+    <>
+      <>
+      <div className='eqStatus'>
+        <div>
+          <Link to='ongoing'>Ongoing</Link>
+        </div>
+        <div>
+          <Link to='completed'>Completed</Link>
+        </div>
+        <div>
+          <Link to='rejected'>Rejected</Link>
+        </div>
+      </div>
+      <div className='eqStatus-outlet'>
+        <Outlet/>
+      </div>
+    </>
+    </>
   )
 }
--- admin/src/components/facilities/FacilityStatus.jsx
+++ admin/src/components/facilities/FacilityStatus.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import '../../styles/eqStatus.css'
+import { Link, Outlet } from 'react-router-dom'
 export default function FacilityStatus() {
   return (
     <>
-      Facility Status
+      <>
+      <div className='eqStatus'>
+        <div>
+          <Link to='ongoing'>Ongoing</Link>
+        </div>
+        <div>
+          <Link to='completed'>Completed</Link>
+        </div>
+        <div>
+          <Link to='rejected'>Rejected</Link>
+        </div>
+      </div>
+      <div className='eqStatus-outlet'>
+        <Outlet/>
+      </div>
+    </>
     </>
   )
 }
