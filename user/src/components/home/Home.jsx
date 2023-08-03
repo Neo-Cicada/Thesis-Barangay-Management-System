8868cb7ee9e3ab3adca126a359957a32f892cd9c
Neo <lordneobarnachea@gmail.com>
2023-08-03 18:06:46 +0800

    adde scrollable box in Home.jsx and customized its scroll bar

--- user/src/components/home/Home.jsx
+++ user/src/components/home/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import PostBox from './PostBox'
+import { Container } from '@mui/material'
+
 export default function Home() {
   return (
     <>
-      <PostBox/>
+      <Container className="custom-scrollbar" style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                height: '100%',
+                overflow: 'auto',
+                paddingTop: '1em',
+                gap: '1em',
+                scrollbarWidth: 'thin',
+                scrollbarColor: '#333 #f1f1f1'
+            }}>
+              <PostBox/>
+              <PostBox/>
+              <PostBox/>
+              <PostBox/>
+              <PostBox/>
+              <PostBox/>
+
+            </Container>
     </>
   )
 }
