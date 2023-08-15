c91d24bf452acfde1d18be8fa48a1f6e4709d068
Neo <lordneobarnachea@gmail.com>
2023-08-15 16:09:44 +0800

    can now upload image

--- admin/src/components/announcement/AnnouncementManagement.jsx
+++ admin/src/components/announcement/AnnouncementManagement.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import {Button, Divider, TextField} from '@mui/material'
+import { Button, Divider, TextField, InputLabel, Input } from '@mui/material'
+import './styles/announcement.css'
+import { useState } from 'react'
+import { storage } from '../../firebase'
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage'
 
-const boxStyle={
+const boxStyle = {
   border: '1px solid white',
   height: '80%',
   width: '50%',
@@ -10,20 +14,45 @@ const boxStyle={
 }
 
 export default function AnnouncementManagement() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+
+  const imageListRef = ref(storage, 'announcement/');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("tap")
+    console.log(imageUpload)
+    if (imageUpload == null) return;
+
+    const imageRef = ref(storage, `announcement/${imageUpload.name}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        if (!imageList.includes(url)) {
+          setImageList((prev) => [...prev, url]);
+        }
+      });
+    });
+
+  }
+
   return (
     <>
-        <div style={boxStyle}>
-          <div style={{width: '100%', height: '10%'}}>Create Post</div>
-          <TextField
-          label="Create Post"
-          multiline
-          rows={10}
-          sx={{ width: '100%' }}
-          defaultValue="Default Value"
-        />
-          <Divider/>
-          <Button sx={{width:'100%'}}>Post</Button>
-        </div>
+
+      <div style={boxStyle}>
+        <form onSubmit={handleSubmit}>
+          <div style={{ width: '100%', height: '10%' }}>Create Post</div>
+          <textarea style={{ width: '100%', maxWidth: '100%', minWidth: '100%', height: '30%' }} placeholder='Enter Announcement' />
+          <TextField type='file' onChange={(e) => setImageUpload(e.target.files[0])} fullWidth />
+          <Divider />
+          <Button sx={{ width: '100%' }} type='submit'>Post</Button>
+        </form>
+      </div>
+
     </>
   )
-}
+};
+
+
+
+
