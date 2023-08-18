d4a39e88c6c39dd29f67564e4a78992ba6b4690c
Neo <lordneobarnachea@gmail.com>
2023-08-18 15:29:51 +0800

    working with file upload

--- user/src/components/home/Home.jsx
+++ user/src/components/home/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import PostBox from './PostBox'
 import { Container } from '@mui/material'
-
+import { useState, useEffect } from 'react';
+import { storage } from '../../firebase'
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage'
 export default function Home() {
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, "images/");
+
+  useEffect(() => {
+    listAll(imageListRef).then((response) => {
+        const urlPromises = response.items.map((item) => {
+            return getDownloadURL(item);
+        });
+        Promise.all(urlPromises).then((urls) => {
+            setImageList(urls);
+        });
+    });
+}, []);
+
+  const imageMap = imageList.map((item)=><PostBox/>);
   return (
     <>
       <Container className="custom-scrollbar" style={{
@@ -16,12 +33,7 @@ export default function Home() {
                 scrollbarWidth: 'thin',
                 scrollbarColor: '#333 #f1f1f1'
             }}>
-              <PostBox/>
-              <PostBox/>
-              <PostBox/>
-              <PostBox/>
-              <PostBox/>
-              <PostBox/>
+              {imageMap}
 
             </Container>
     </>
