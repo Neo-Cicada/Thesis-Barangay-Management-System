0c02ca1f7a352ae84108377216861d189b5a40f9
Neo <lordneobarnachea@gmail.com>
2023-08-19 15:09:21 +0800

    can now add announcement as metadata

--- admin/src/components/announcement/AnnouncementManagement.jsx
+++ admin/src/components/announcement/AnnouncementManagement.jsx
@@ -3,7 +3,7 @@ import { Button, Divider, TextField, InputLabel, Input } from '@mui/material'
 import './styles/announcement.css'
 import { useState } from 'react'
 import { storage } from '../../firebase'
-import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage'
+import { ref, uploadBytes, listAll, getDownloadURL, updateMetadata } from 'firebase/storage'
 
 const boxStyle = {
   border: '1px solid white',
@@ -16,33 +16,57 @@ const boxStyle = {
 export default function AnnouncementManagement() {
   const [imageUpload, setImageUpload] = useState(null);
   const [imageList, setImageList] = useState([]);
-
+  const [message, setMessage] = useState("");
   const imageListRef = ref(storage, 'announcement/');
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log("tap")
-    console.log(imageUpload)
+  const newMetadata = {
+    customMetadata: {
+      "announcement": { message }
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("tap");
+    console.log(message)
     if (imageUpload == null) return;
 
-    const imageRef = ref(storage, `announcement/${imageUpload.name}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        if (!imageList.includes(url)) {
-          setImageList((prev) => [...prev, url]);
-        }
-      });
-    });
+    try {
+      const imageRef = ref(storage, `announcement/${imageUpload.name}`);
 
-  }
+      // Uploading image bytes
+      const snapshot = await uploadBytes(imageRef, imageUpload);
 
+      // Getting download URL and updating metadata
+      const url = await getDownloadURL(snapshot.ref);
+      if (!imageList.includes(url)) {
+        setImageList((prev) => [...prev, url]);
+      }
+
+      // Updating metadata
+      const newMetadata = {
+        customMetadata: {
+          // Add your custom metadata fields here
+          announcement: message,
+          timestamp: new Date().toISOString(),
+        },
+      };
+      await updateMetadata(imageRef, newMetadata);
+
+      console.log("Upload successful");
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  };
   return (
     <>
 
       <div style={boxStyle}>
         <form onSubmit={handleSubmit}>
           <div style={{ width: '100%', height: '10%' }}>Create Post</div>
-          <textarea style={{ width: '100%', maxWidth: '100%', minWidth: '100%', height: '30%' }} placeholder='Enter Announcement' />
+          <textarea
+            style={{ width: '100%', maxWidth: '100%', minWidth: '100%', height: '30%' }}
+            placeholder='Enter Announcement'
+            onChange={(e) => setMessage(e.target.value)}
+          />
           <TextField type='file' onChange={(e) => setImageUpload(e.target.files[0])} fullWidth />
           <Divider />
           <Button sx={{ width: '100%' }} type='submit'>Post</Button>
