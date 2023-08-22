8af7772ff27cc4876327f3166ac6f529f61cb2b7
Neo <lordneobarnachea@gmail.com>
2023-08-22 20:11:44 +0800

    changed color of assets

--- user/src/formComponents/EnrollmentForm.jsx
+++ user/src/formComponents/EnrollmentForm.jsx
@@ -5,27 +5,6 @@ import { getStorage, ref } from "firebase/storage";
 
 
 export default function EnrollmentForm() {
-  // Get a reference to the storage service, which is used to create references in your storage bucket
-  const storage = getStorage();
-
-  // Create a storage reference from our storage service
-  const storageRef = ref(storage);
-  // Points to 'images'
-const imagesRef = ref(storageRef, 'images');
-
-// Points to 'images/space.jpg'
-// Note that you can use variables to create child values
-const fileName = 'space.jpg';
-const spaceRef = ref(imagesRef, fileName);
-
-// File path is 'images/space.jpg'
-const path = spaceRef.fullPath;
-
-// File name is 'space.jpg'
-const name = spaceRef.name;
-
-// Points to 'images'
-const imagesRefAgain = spaceRef.parent;
 
   return (
     <>
