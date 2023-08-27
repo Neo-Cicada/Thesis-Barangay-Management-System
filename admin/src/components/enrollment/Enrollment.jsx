99dc102e199af91aa0116fc0c7ac5fdb32a6d40b
Neo <lordneobarnachea@gmail.com>
2023-08-27 18:14:52 +0800

    working with enrolment component

--- admin/src/components/enrollment/Enrollment.jsx
+++ admin/src/components/enrollment/Enrollment.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { Container, Box, Button } from '@mui/material'
 export default function Enrollment() {
   return (
-    <div>
-      Enrollment
-    </div>
+    <>
+      <Container sx={{
+          border: '1px solid red',
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column'
+          }}>
+
+        <Box sx={{border:'1px solid black', height:'90%', display: 'flex'}}>
+          <Box sx={{width: '70%', border: '1px solid red'}}> {/*box for request */}
+
+          </Box>
+          <Box sx={{width: '30%', border: '1px solid red', display:'flex', justifyContent:'center', gap:'1em'}}>
+            <Box>Enrolled</Box>
+            <Box>Rejected</Box>
+          </Box>
+        </Box>
+      </Container>
+    </>
   )
 }
