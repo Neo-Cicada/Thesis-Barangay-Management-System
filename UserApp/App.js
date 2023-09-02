53e4f575c9a7a5e3b440d453880b9f2d1aaeba28
Neo <lordneobarnachea@gmail.com>
2023-09-02 22:02:14 +0800

    started working with the header

--- UserApp/App.js
+++ UserApp/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world! from react native!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.headerContainer}>
+        <Header />
+      </View>
     </View>
   );
 }
@@ -13,8 +15,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f7f7f7', // Set the background color
+  },
+  headerContainer: {
+    flex: 0.1,
+    borderColor: 'red', // Set border color
+    borderWidth: 1,     // Set border width
+    alignItems: 'center',     // Center horizontally
+    justifyContent: 'center', // Center vertically
   },
 });
