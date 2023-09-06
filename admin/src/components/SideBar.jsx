9e6cc3a751e1d9da8faca1d03033d2e737f9dff0
Neo <lordneobarnachea@gmail.com>
2023-09-06 22:12:33 +0800

    done with sidebar design

--- admin/src/components/SideBar.jsx
+++ admin/src/components/SideBar.jsx
@@ -7,6 +7,24 @@ import { useState } from 'react'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
+import LocalHospitalOutlinedIcon from '@mui/icons-material/LocalHospitalOutlined';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+
+import HouseOutlinedIcon from '@mui/icons-material/HouseOutlined';
+import HouseIcon from '@mui/icons-material/House';
+
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
+
+import CampaignOutlinedIcon from '@mui/icons-material/CampaignOutlined';
+import CampaignIcon from '@mui/icons-material/Campaign';
+
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+
+import HandymanOutlinedIcon from '@mui/icons-material/HandymanOutlined';
+import HandymanIcon from '@mui/icons-material/Handyman';
+
 export default function SideBar() {
   // const loc = useLocation();
   // const path = loc.pathname; scan current location
@@ -24,7 +42,7 @@ export default function SideBar() {
 
   const activeStyle = {
     ...normalStyle,
-    fontWeight: 'bold',
+    fontWeight: 'bolder',
     fontSize: '1.1rem',
 
 
@@ -34,8 +52,8 @@ export default function SideBar() {
   return (
     <>
       <div className='nav-container'>
-        <div style={{ border: '1px solid red', height: '10%', marginTop: '15%' }}>
-          <h1 >Logo</h1>
+        <div style={{  height: '10%', marginTop: '15%' }}>
+          {/* <h1 >Logo</h1> */}
 
         </div>
         <ul>
@@ -46,7 +64,11 @@ export default function SideBar() {
               sx={status === "equipment" ? activeStyle : normalStyle}
               onClick={() => setStatus("equipment")}
             >
-              <div className='sidebar-logo'>Logo</div>
+              <div className='sidebar-logo'>
+                {
+                  status === "equipment" ? <HandymanIcon fontSize="large" /> : <HandymanOutlinedIcon fontSize="large" />
+                }
+              </div>
               <div className='sidebar-btn-name'>Equipment</div>
             </Button>
           </li>
@@ -56,7 +78,11 @@ export default function SideBar() {
             sx={status === "certificate" ? activeStyle : normalStyle}
             onClick={() => setStatus("certificate")}
           >
-            <div className='sidebar-logo'>Logo</div>
+            <div className='sidebar-logo'>
+              {
+                status === "certificate" ? <ReceiptIcon fontSize="large" /> : <ReceiptOutlinedIcon fontSize="large" />
+              }
+            </div>
             <div className='sidebar-btn-name'>Certification</div>
           </Button></li>
           <li>
@@ -67,7 +93,11 @@ export default function SideBar() {
               sx={status === "announcement" ? activeStyle : normalStyle}
               onClick={() => setStatus("announcement")}
             >
-              <div className='sidebar-logo'>Logo</div>
+              <div className='sidebar-logo'>
+                {
+                  status === "announcement" ? <CampaignIcon fontSize="large" /> : <CampaignOutlinedIcon fontSize="large" />
+                }
+              </div>
               <div className='sidebar-btn-name'>Announcement</div>
             </Button>
           </li>
@@ -77,7 +107,10 @@ export default function SideBar() {
               sx={status === "enrollment" ? activeStyle : normalStyle}
               onClick={() => setStatus("enrollment")}
             >
-              <div className='sidebar-logo'>Logo</div>
+              <div className='sidebar-logo'>{
+                status === "enrollment" ?
+                  <LocalLibraryIcon fontSize="large" /> :
+                  <LocalLibraryOutlinedIcon fontSize="large" />}</div>
               <div className='sidebar-btn-name'>Enrollment</div>
             </Button>
           </li>
@@ -87,7 +120,9 @@ export default function SideBar() {
               sx={status === "facilities" ? activeStyle : normalStyle}
               onClick={() => setStatus("facilities")}
             >
-              <div className='sidebar-logo'>Logo</div>
+              <div className='sidebar-logo'>{status === "facilities" ? 
+              <HouseIcon fontSize="large" /> :
+               <HouseOutlinedIcon fontSize="large" />}</div>
               <div className='sidebar-btn-name'>Facilities</div>
             </Button>
           </li>
@@ -97,7 +132,8 @@ export default function SideBar() {
               sx={status === "medicine" ? activeStyle : normalStyle}
               onClick={() => setStatus("medicine")}
             >
-              <div className='sidebar-logo'>Logo</div>
+              <div className='sidebar-logo'>{status === "medicine" ?
+              <LocalHospitalIcon fontSize="large" /> : <LocalHospitalOutlinedIcon fontSize="large"/>}</div>
               <div className='sidebar-btn-name'>Medicine</div>
             </Button>
           </li>
@@ -110,8 +146,8 @@ export default function SideBar() {
             >
               <div className='sidebar-logo'>
                 {status === "profile" ?
-                    <AccountCircleIcon fontSize='large' />
-                 : <AccountCircleOutlinedIcon fontSize="large" /> }
+                  <AccountCircleIcon fontSize='large' />
+                  : <AccountCircleOutlinedIcon fontSize="large" />}
               </div>
               <div className='sidebar-btn-name'>Profile</div>
             </Button>
