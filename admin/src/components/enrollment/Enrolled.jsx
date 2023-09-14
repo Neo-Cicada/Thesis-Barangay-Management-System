02918e324adb6f4a691715ed77c28b9bb04ab947
Neo <lordneobarnachea@gmail.com>
2023-09-14 21:32:39 +0800

    refractoring enrollment component

--- admin/src/components/enrollment/Enrolled.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import useRead from '../../hooks/useRead'
-import {Container, Box} from "@mui/material"
-import { useState } from 'react';
-export default function Enrolled() {
-  return
-}
--- admin/src/components/enrollment/Enrollment.jsx
+++ admin/src/components/enrollment/Enrollment.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import { Container, Box, Button } from '@mui/material'
-import EnrollmentRequest from './EnrollmentRequest'
-import {useState} from 'react'
-import Enrolled from './Enrolled'
-import EnrollmentRejected from './EnrollmentRejected'
+import { useState } from 'react'
+import ChecklistIcon from '@mui/icons-material/Checklist'
+import DashboardBox from '../DashboardBox'
+import DashboardNavigation from '../DashboardNavigation'
 export default function Enrollment() {
-  const [status, setStatus] = useState(true)
+  const [status, setStatus] = useState('default')
   return (
     <>
-
+      <Container style={{
+        height: '30%', display: 'flex',
+        flexDirection: 'column', gap: '1em',
+      }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <h3>Manage Enrollments</h3>
+        </div>
+        <div style={{ display: 'flex', gap: '1em' }}>
+        <DashboardBox
+            name="Total Request"
+            numbers={'1'}
+            logo={<ChecklistIcon />} />
+          <DashboardBox
+            name="Ongoing"
+            numbers={'2'}
+            logo={<ChecklistIcon />} />
+          <DashboardBox
+            name="Completed"
+            numbers={'3'}
+            logo={<ChecklistIcon />} />
+          <DashboardBox
+            name="Rejected"
+            numbers={'4'}
+            logo={<ChecklistIcon />} />
+          <DashboardBox
+            name="Enrolled"
+            numbers={'888'}
+            logo={<ChecklistIcon />} />
+        </div>
+      </Container>
+      <DashboardNavigation setStatus={setStatus} status={status} />
     </>
   )
 }
