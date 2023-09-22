814da87c773779c419efee5ead009851ceae6a5d
Neo <lordneobarnachea@gmail.com>
2023-09-22 18:31:39 +0800

    working with services and added svgs

--- user/src/newComponents/services/ServiceBox.jsx
+++ user/src/newComponents/services/ServiceBox.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './services.css'
-export default function ServiceBox() {
+
+export default function ServiceBox({title, image}) {
   return (
     <>
       <div className='service-box' >
-        
+        <img src={image} width={'100%'} height={'80%'}/>
+        <p className='service-box-title'>{title}</p>
+
       </div>
     </>
   )
--- user/src/newComponents/services/Services.jsx
+++ user/src/newComponents/services/Services.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import './services.css'
 import ServiceBox from './ServiceBox'
+import MedicineIcon from '../../assets/medicine.svg'
+import FacilityIcon from '../../assets/facility.svg'
+import CertificateIcon from '../../assets/certificate.svg'
+import EnrollmentIcon from '../../assets/enrollment.svg'
+import ReportIcon from '../../assets/report.svg'
+import Equipment from '../../assets/equipment.svg'
 export default function Services() {
   return (
     <>
       <div className='services'>
         <p className='services-title'>Services</p>
         <div className='service-body'>
-            <ServiceBox/>
-            <ServiceBox/><ServiceBox/><ServiceBox/><ServiceBox/>
-            <ServiceBox/>
+            <ServiceBox title={'Medicines'} image={MedicineIcon}/>
+            <ServiceBox title={'Certificates'} image={CertificateIcon}/>
+            <ServiceBox title={'Facility'} image={FacilityIcon}/>
+            <ServiceBox title={'Enrollment'} image={EnrollmentIcon}/>
+            <ServiceBox title={'Report'} image={ReportIcon}/>
+            <ServiceBox title={'Equipment'} image={Equipment}/>
         </div>
       </div>
     </>
