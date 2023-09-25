b5e27187698cf8e1f3561167cc630d2bcebd5d48
Neo <lordneobarnachea@gmail.com>
2023-09-25 20:25:21 +0800

    can now read post images

--- user/src/newComponents/announcement/Announcement.jsx
+++ user/src/newComponents/announcement/Announcement.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import './announcement.css'
 import AnnouncementBox from './AnnouncementBox'
+import useRead from '../../hooks/useRead'
 export default function Announcement() {
+  const [data, setData] = useState([])
   const [isExpand, setIsExpand] = useState(false)
+  useRead('images', setData)
+  console.log(data)
+  const items = data.map(item=><AnnouncementBox image={item.data.imageUrl} description={item.data.description}/>)
   const scrollable = {
     overflow: 'auto'
   }
@@ -12,10 +17,7 @@ export default function Announcement() {
       <div className='announcement' id='announcement'>
         <p className='announcement-title'>Announcements</p>
         <div className='announcement-body' style={style}>
-            <AnnouncementBox/>
-            <AnnouncementBox/>
-            <AnnouncementBox/>
-            <AnnouncementBox/>
+            {items}
         </div>
         <div className='announcement-btn'>
             <button onClick={()=>setIsExpand(!isExpand)}>{isExpand ? 'See less': 'See more'}</button>
--- user/src/newComponents/announcement/AnnouncementBox.jsx
+++ user/src/newComponents/announcement/AnnouncementBox.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import './announcement.css'
 import {Button} from '@mui/material'
-export default function AnnouncementBox() {
+export default function AnnouncementBox({image, description, date}) {
   return (
     <>
       <div className='announcementBox'>
         <div className='announcement-image'>
-          image
+          <img src={image} height='100%' width={'100%'}/>
         </div>
-        <div className='announcement-text' >Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Voluptatem ratione officiis ex laboriosam cumque, adipisci sint. Aspernatur
-          ea distinctio quisquam vero doloribus
-          assumenda, nisi obcaecati repudiandae, dolore laudantium voluptates neque.</div>
+        <div className='announcement-text' >{description}</div>
         <div className='announcement-footer' >
           <div className='date'>08/27/2023</div>
           <Button className='btn'>View</Button>
