2d8e8d5cf12d0056d9d72a50cca34cb06d8edae6
Neo <lordneobarnachea@gmail.com>
2023-10-09 11:31:24 +0800

    gix fix fix

--- admin/src/components/dashboard/Dashboard.jsx
+++ admin/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import MedicineChart from './MedicineChart'
+import './dashboard.css'
 export default function Dashboard() {
   return (
-    <div>
-      Dashboard
-    </div>
+    <>
+      <div className='dashboard-container'>
+        <h2 style={{height:'10%', display:'flex', alignItems:'center', justifyContent:'center'}}>Dashboard</h2>
+        <div className='dashbox'>
+          <div className='grap-one'>a graph that show what services is the most request something like that</div>
+          <div className='grap-two'>Most Requested items</div>
+
+          <div className='grap-three'>graph for each services where in shows acceptance rante and blahblha</div>
+          <div className='grap-forth'>4boxes of all accepted, rejected,ongoing,rejeted</div>
+
+
+        </div>
+      </div>
+    </>
   )
 }
---
+++ admin/src/components/dashboard/EnrollmentChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EnrollmentChart() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
