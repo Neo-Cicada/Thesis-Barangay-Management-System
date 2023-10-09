895a212d3c078a41e32997157229625cf359e179
Neo <lordneobarnachea@gmail.com>
2023-10-09 22:26:36 +0800

    added a bug

--- admin/src/components/dashboard/Dashboard.jsx
+++ admin/src/components/dashboard/Dashboard.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import MedicineChart from './MedicineChart'
 import { Container } from '@mui/material'
 import './dashboard.css'
+import FacilityChart from './FacilityChart'
+import { BigChart } from './BigChart'
 export default function Dashboard() {
   return (
     <>
       <Container className='dashboard-container'>
         <h2 style={{height:'10%', display:'flex', alignItems:'center', justifyContent:'center'}}>Dashboard</h2>
         <div className='dashbox'>
-          <div className='grap-one'>a graph that show what services is the most request something like that</div>
-          <div className='grap-two'>Most Requested items</div>
+          <div className='grap-one'><BigChart/></div>
+          <div className='grap-two'><MedicineChart/></div>
 
-          <div className='grap-three'>graph for each services where in shows acceptance rante and blahblha</div>
+          <div className='grap-three'><FacilityChart/></div>
           <div className='grap-forth'>4boxes of all accepted, rejected,ongoing,rejeted</div>
 
 
