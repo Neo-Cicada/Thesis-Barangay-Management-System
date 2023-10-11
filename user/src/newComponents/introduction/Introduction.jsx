4d22a8fb74b02d0cf7ae4a1485b8ca079ff65b95
Neo <lordneobarnachea@gmail.com>
2023-10-11 22:27:30 +0800

    added image background

--- user/src/newComponents/introduction/Introduction.jsx
+++ user/src/newComponents/introduction/Introduction.jsx
@@ -5,11 +5,14 @@ export default function Introduction() {
     <>
       <div className='introduction' id='home'>
         <div className='intro-discription-box'>
-          <p className='intro-title'>
+          <p className='intro-title' style={{fontSize:'3rem', fontWeight:'bolder'}}>
             BARANGAY AMAMPEREZ
           </p>
-          <p className='intro-location'>Villasis Pangasinan</p>
-          <p className='intro-description'><span style={{color:'#3B5998', fontSize:'2rem'}}><b>Welcome to eBarangay</b></span> <br/>
+          <p style={{fontWeight:'bolder',}} className='intro-location'>Villasis Pangasinan</p>
+          <p className='intro-description' style={{fontWeight:'bolder', fontSize:'1.6rem'}}><span style={{color:'white', fontSize:'2rem', borderBottom:'8px solid #3B5998' }}>
+            <b>Welcome to eBarangay</b>
+            </span> 
+            <br/>
             eBarangay is the ultimate solution for streamlined and efficient barangay management. Say goodbye to paperwork and long queues.
             With eBarangay, you can access a wide range of online services and stay informed with ease.
           </p>
