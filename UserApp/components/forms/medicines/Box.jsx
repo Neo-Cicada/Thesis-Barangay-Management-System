ecd87590eaa7fcca02f71a2738a8c6be4f913bb2
Neo <lordneobarnachea@gmail.com>
2023-10-17 07:58:34 +0800

    fix fix fix

--- UserApp/components/forms/medicines/Box.jsx
+++ UserApp/components/forms/medicines/Box.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-export default function Box() {
-    return (
-        <>
-            <View style={{
-                width: 100, height: 100, borderRadius: 100, backgroundColor: 'green',
-                borderWidth: 1, borderColor: 'red', flex: 1
-            }}>
-                <Text>Hello</Text>
-            </View>
-        </>
-    )
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+function Box({ name, isSelected, onSelect, quantity }) {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.box,
+        { backgroundColor: isSelected ? '#8B9DC3' : '#DFE3EE' },
+      ]}
+      onPress={() => onSelect(name)}
+    >
+      <Text style={styles.nameText}>{name}</Text>
+      <Text>{quantity}</Text>
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    height: 120, // 5em in pixels
+    width: 140,  // 7em in pixels
+    borderRadius: 10, // 1em in pixels
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+  nameText: {
+    fontSize: 20, // 1em in pixels
+  },
+});
+
+export default Box;
--- UserApp/components/forms/medicines/Medicine.jsx
+++ UserApp/components/forms/medicines/Medicine.jsx
@@ -1,38 +1,78 @@
 import React, { useState, useContext, createContext, useEffect } from 'react'
-import { ScrollView, Text, View, StyleSheet } from 'react-native'
+import { ScrollView, Text, View, StyleSheet, Pressable, TextInput } from 'react-native'
 import Box from './Box';
 
 export const MyMedicineContext = createContext();
 
 export default function Medicine() {
   const [selectedItems, setSelectedItems] = useState([]);
-
-  const handleBoxSelection = (value) => {
-    // Check if the value is already in the selectedItems array
-    if (selectedItems.includes(value)) {
-      // If it's already selected, deselect it
-      setSelectedItems(selectedItems.filter(item => item !== value));
+  const handleBoxSelection = (name) => {
+    if (selectedItems.some(item => item.name === name)) {
+      // If the item is already selected, deselect it
+      setSelectedItems(selectedItems.filter(item => item.name !== name));
     } else {
-      // If it's not selected, select it
-      setSelectedItems([...selectedItems, value]);
+      // If the item is not selected, select it with an initial quantity of 0
+      setSelectedItems([...selectedItems, { name, count: 0 }]);
     }
+  };
+  const handleQuantityChange = (name, newCount) => {
+    const updatedSelectedItems = selectedItems.map((item) => {
+      if (item.name === name) {
+        return { ...item, count: parseInt(newCount) || 0 };
+      }
+      return item;
+    });
+    setSelectedItems(updatedSelectedItems);
   };
   return (
     <>
       <MyMedicineContext.Provider>
         <ScrollView>
           <View style={{ borderWidth: 1, borderColor: 'red', height: 500 }}>
-            <ScrollView  nestedScrollEnabled={true} style={{ flex: 0.5, height:300, borderWidth: 1, borderColor: 'red' }}>
+            <ScrollView nestedScrollEnabled={true} style={{ flex: 0.5, height: 300, borderWidth: 1, borderColor: 'red' }}>
               {/* options */}
-              <TouchableOpacity onPress={() => handleBoxSelection(1)}>
-            <Box selected={selectedItems.includes(1)} />
-          </TouchableOpacity>
-              <Box/>
-              <Box/>
-              <Box/>
+
+              <Box
+                name="box"
+                quantity="11"
+                isSelected={selectedItems.some(item => item.name === "box")}
+                onSelect={handleBoxSelection}
+              />
+              <Box
+                name="box2"
+                quantity="11"
+                isSelected={selectedItems.some(item => item.name === "box2")}
+                onSelect={handleBoxSelection}
+              />
+
+              <Box
+                name="box3"
+                quantity="11"
+                isSelected={selectedItems.some(item => item.name === "box3")}
+                onSelect={handleBoxSelection}
+              />
             </ScrollView>
-            <ScrollView style={{ flex: 0.5, borderWidth: 1, borderColor: 'red' }}>
+            <ScrollView style={{ flex: 0.5, borderWidth: 1, borderColor: 'black', }}>
               {/* selected options */}
+              {selectedItems.map((medicine, index) => (
+                <View key={index} style={{
+                  display:'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+
+                }}>
+                  <Text style={{ textAlign: 'center' }}>{medicine.name}</Text>
+                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <TextInput
+                      // style={styles.quantityInput}
+                      placeholder="Enter quantity"
+                      keyboardType="numeric"
+                      value={medicine.count.toString()} onChangeText={(text) => handleQuantityChange(medicine.name, text)}
+                    />
+                  </View>
+                </View>
+              ))}
             </ScrollView>
           </View>
           <View style={{ borderWidth: 1, borderColor: 'red', height: 550 }}>
